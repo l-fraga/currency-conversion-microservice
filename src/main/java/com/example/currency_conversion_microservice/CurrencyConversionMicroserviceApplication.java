549b2d7c7ced98f47cb9efdc0e22310f5e9d77a7
Lucas Silva <lucassilvacma@hotmail.com>
2024-10-28 09:47:11 -0300

    setting the environment

---
+++ src/main/java/com/example/currency_conversion_microservice/CurrencyConversionMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.currency_conversion_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionMicroserviceApplication.class, args);
+	}
+
+}
